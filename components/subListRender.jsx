72279e3ddc08c430705fae92c2d0097c647136ae
Paul <prose0021@gmail.com>
2020-04-06 11:22:36 -0700

    add highlighting

--- components/subListRender.jsx
+++ components/subListRender.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSubreddits from './subListHook';
 
 const SubList = ({ handleClick }) => {
     
     const { subredditList } = useSubreddits({});
+    const [selected, setSelected] = useState("")
+
+    function updateStyling(info) {
+      let id = info.data.id
+      if (selected) {
+        let el = document.getElementById(selected);
+        el.classList.remove('selected');
+      }
+      setSelected(id)
+      let newEl = document.getElementById(id);
+      console.log(newEl)
+      newEl.classList.add('selected')
+      handleClick(info.data.url)
+    }
 
     return subredditList 
     ? (
         <ul className="subreddit-list">
                 {subredditList.map(
-                info => {
+                  info => {
+                        const id = info.data.id
                         const image = info.data.icon_img ? (
                           <img src={info.data.icon_img} />
                         ) : (
                           <img src="https://github.com/rose-paul/browse_reddit/blob/master/public/react_reddit.png?raw=true" />
                         );
                     return (
-                      <ul onClick={() => handleClick(info.data.url)}>
+                      <ul id={id} onClick={() => updateStyling(info)}>
                           <li>
                             <b>{info.data.url}</b>
                           </li>
