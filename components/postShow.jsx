abc0b4acb30182913ec0b5eb825ab6bd5774a7cd
Paul <prose0021@gmail.com>
2020-04-12 18:15:38 -0700

    modal css

--- components/postShow.jsx
+++ components/postShow.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 
 const PostShow = ({ data }) => {
 
-    const image = data.url ? data.url : "";
+    const image = data.url ? (
+      <li>
+        <img src={data.url} id="post-show-img" />
+      </li>
+    ) : (
+      null
+    );
+
+    const text = data.selftext ? (
+      <li>{data.selftext}</li>
+    ) : (null);
 
     return data ? (
       <ul>
-        <li>Posted by  {data.author}</li>
+        <li>Posted by {data.author}</li>
         <li>
           <b>{data.title}</b>
         </li>
-        <li>{data.selftext}</li>
-        <li>
-          <img src={image} alt="" />
-        </li>
+        {text}
+        {image}
       </ul>
     ) : (
       <p>getting post</p>
